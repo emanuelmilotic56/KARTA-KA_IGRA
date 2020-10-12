4ebdec4f311115c314748be87a7e82e2a3335689
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2020-10-12 08:49:56 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KARTAŠKA_IGRA_EMANUEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Karte = 0;
+            int CardCounter = "0";
+
+
+            while (CardCounter < 31)
+            {
+
+
+                Console.WriteLine(Unesi kartu od 1 do 13);
+
+
+
+            }
+
+            if (Karte > 13)
+            {
+                Console.WriteLine("Vas broj je veći od 13");
+            }
+            else
+            {
+
+                CardCounter + = Karte;
+            }
+
+        }
+          if (CardCounter< 30){
+
+            Console.Writeline("Pobjeda!"+CardCounter++);
+
+           
+    }
+}
